88ae2a98b70353265d6a31ee2b137fc2fa5d00e9
otinu <fitshter@gmail.com>
2022-06-05 14:39:35 +0900

    test_commit

--- Numpy.py
+++ Numpy.py
@@ -7,11 +7,10 @@ import numpy as np
 a = np.array([1, 2, 3])
 print(a)
 print(type(a))
-print(a.shape) # 1次元配列で3要素ある
+print(a.shape)  # 1次元配列で3要素ある
 
 """
 [1 2 3]
 <class 'numpy.ndarray'>
 (3,)
-
 """
