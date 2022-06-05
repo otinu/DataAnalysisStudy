1c0258e5a29f95537efd18914974e8a4178f1d10
otinu <fitshter@gmail.com>
2022-06-05 18:35:23 +0900

    [fix]Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -14,3 +14,31 @@ print(a.shape)  # 1次元配列で3要素ある
 <class 'numpy.ndarray'>
 (3,)
 """
+# 2次元配列
+b = np.array([[1, 2, 3], [4, 5, 6]])
+print(b.shape)
+# ⇒ (2, 3)
+
+# 次元変換==================================================
+c1 = np.array([0, 1, 2, 3, 4, 5])
+
+## reshape()
+
+# 2×3に変換
+c2 = c1.reshape((2, 3))
+print(c2)
+
+"""
+[[0 1 2]
+ [3 4 5]]
+ """
+
+## ravel() ⇒ 参照返し
+## flatten() ⇒ コピー返し
+
+# 1次元に戻す
+c3 = c2.ravel()
+print(c3)
+
+c4 = c2.flatten()
+print(c4)
