2b8b5c7ea3870fe705b7ddd177dbd260b0eeba60
otinu <fitshter@gmail.com>
2022-06-08 21:09:59 +0900

    [fix]Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -16,3 +16,17 @@ fig, ax = plt.subplots()
 ax.plot(x[0] + 1, y[0] - 1)
 ax.set_title("OOP-style")
 plt.show()
+
+#=======================================
+
+## サブプロット2個
+fig, axes = plt.subplots(2)
+plt.show()
+
+## サブプロット2×2個
+fig, axes = plt.subplots(2, 2)
+plt.show()
+
+## サブプロット1×2個
+fig, axes = plt.subplots(ncols=2)
+plt.show()
