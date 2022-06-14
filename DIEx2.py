1ab75d21ca2e28995f87ffb03d5943764b1408df
otinu <fitshter@gmail.com>
2022-06-14 21:07:47 +0900

    [add]DIEx.py

---
+++ DIEx2.py
@@ -0,0 +1,82 @@
+"""
+ランダムフォレストは他の機械学習アルゴリズムと比較すると、欠損値の穴埋めや標準化などのデータの前処理を必要としないアルゴリズム
+
+行列Aのサイズが(n,m)、行列Bのサイズが（m,n）のとき、この２つの行列の積ABのサイズとして正しいもの
+⇒(n, n)
+
+データサイエンティストは【論文の読解能力】必要
+データエンジニアは不要(テキスト曰く)
+
+df.set_index() ⇒ インデックスの設置
+df.reset_index() ⇒ インデックスの再配置(追加や削除もこれ)
+
+行列の計算
+https://atarimae.biz/archives/23930
+
+"""
+
+"""
+A = [x for x in range(10)]
+B = {x for x in range(20)}
+
+if len(A)==10:
+    print("A")
+elif len(A)==10&len(B)==20:
+    print("A,B")
+else:
+    print("None")
+
+【結果】
+A
+⇒要確認
+"""
+"""
+ある変数xの標準偏差が1、ある変数yの標準偏差が2、この2つのxとyの共分散が2であるとき、この2変数のピアソンの相関係数として正しいものを選べ。
+⇒1
+　⇒要確認
+
+"""
+
+"""
+np.random.normal() ⇒ 平均loc、標準偏差scaleの正規分布に従う乱数を返す。
+np.random.randint() ⇒ 任意の範囲で整数を返す。任意範囲でN行列の生成も可能
+np.random.randn() ⇒ 平均0、分散1（標準偏差1）の正規分布（標準正規分布）に従う乱数を返す。
+np.random.uniform() ⇒ 任意の範囲で浮動小数点(float型)を生成
+★np.random.rand() ⇒ 0.0～1.0までの乱数生成
+※下記は全て、★と同じ内容のエイリアス
+np.random.random()
+np.random.random()
+np.random.ranf()
+np.random.sample()
+np.random.random_sample()
+"""
+
+import re
+import pandas as pd
+
+pattern = r"enjoy"
+text = "Let's joy data science"
+matchOB = re.match(pattern, text)
+print(matchOB) # None
+
+# パターンマッチしていた場合、group()で該当部分を取得できる===========================
+text = "enjoy data science"
+matchOB = re.match(pattern, text)
+print(matchOB) # <re.Match object; span=(0, 5), match='enjoy'>
+print(matchOB.group()) # enjoy
+
+# apply()によるデータの追加===========================================================
+df = pd.DataFrame({
+    "col_A": [100, 300, 200, 400, 200, 700, 200],
+    "col_B": ["A", "B", "C", "D", "E", "D", "C"],
+    "col_C": [2, 3, 5, 1, 2, 3, 8]
+})
+print(df.head())
+print()
+
+def func1(row):
+    return row["col_A"] * row["col_C"]
+
+# dfをレコード単位でfunc1に渡して何らかの操作をしてもらう。戻り値は新たに作成する"col_D"に保存する。
+df["col_D"] = df.apply(func1, axis=1)
+print(df.head())
