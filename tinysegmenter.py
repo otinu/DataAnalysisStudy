5867714ebec2aa1abf686582a3065f5039661a15
otinu <fitshter@gmail.com>
2022-06-24 22:58:30 +0900

    [fix]tinysegmenter.py

--- tinysegmenter.py
+++ tinysegmenter.py
@@ -1,7 +1,7 @@
 import newspaper
 
 # サイトによってはスクレイピングは禁止されているため、事前に確認をすること
-url = ""
+url = "●●"
 
 website = newspaper.build(url)
 i =0
@@ -16,3 +16,12 @@ for article in website.articles:
     if i > 9:
         break
     i = i + 1
+
+import csv
+import datetime
+
+csv_date = datetime.datetime.today().strftime("%Y%m%d")
+csv_file_name = "bloomberg_" + csv_date + ".csv"
+f = open(csv_file_name, "w", encoding="cp932", error="ignore")
+writer = csv.writer(f, lineterminator="\n")
+csv_header = ["記事番号", "タイトル", "URL", "サマリー"]
