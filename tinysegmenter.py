804c4c66078136b20dc27c00235ef7338cefd85f
otinu <fitshter@gmail.com>
2022-06-25 17:24:35 +0900

    [fix]tinysegmenter.py

--- tinysegmenter.py
+++ tinysegmenter.py
@@ -6,12 +6,18 @@ url = "●●"
 website = newspaper.build(url)
 i =0
 for article in website.articles:
+    csvlist = []
     article.download()
     article.parse()
     article.nlp()
     print("記事", str(i), ":", article.title)
     print(article.url)
     print(article.summary, ned="\n\n")
+    csvlist.append(str(i))
+    csvlist.append(article.title)
+    csvlist.append(article.url)
+    csvlist.append(article.summary)
+    writer.writerow(csvlist)
 
     if i > 9:
         break
