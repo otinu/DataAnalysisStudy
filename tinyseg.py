8018c29cd94c50bc9efe21ddbb8eea76c56852f8
otinu <fitshter@gmail.com>
2022-06-26 16:40:36 +0900

    [fix]tinyseg.py

--- tinyseg.py
+++ tinyseg.py
@@ -1,9 +1,18 @@
 import newspaper
+import csv
+import datetime
 
 # サイトによってはスクレイピングは禁止されているため、事前に確認をすること
-url = "●●"
+url = "〇〇"
 
-website = newspaper.build(url)
+csv_date = datetime.datetime.today().strftime("%Y%m%d")
+csv_file_name = "bloomberg_" + csv_date + ".csv"
+f = open(csv_file_name, "w", encoding="cp932", errors="ignore")
+csv_header = ["記事番号", "タイトル", "URL", "サマリー"]
+writer = csv.writer(f, lineterminator="\n")
+writer.writerow(csv_header)
+
+website = newspaper.build(url, memoize_articles = False)
 i =0
 for article in website.articles:
     csvlist = []
@@ -12,7 +21,7 @@ for article in website.articles:
     article.nlp()
     print("記事", str(i), ":", article.title)
     print(article.url)
-    print(article.summary, ned="\n\n")
+    print(article.summary, end="\n\n")
     csvlist.append(str(i))
     csvlist.append(article.title)
     csvlist.append(article.url)
@@ -22,12 +31,6 @@ for article in website.articles:
     if i > 9:
         break
     i = i + 1
+f.close()
 
-import csv
-import datetime
 
-csv_date = datetime.datetime.today().strftime("%Y%m%d")
-csv_file_name = "bloomberg_" + csv_date + ".csv"
-f = open(csv_file_name, "w", encoding="cp932", error="ignore")
-writer = csv.writer(f, lineterminator="\n")
-csv_header = ["記事番号", "タイトル", "URL", "サマリー"]
