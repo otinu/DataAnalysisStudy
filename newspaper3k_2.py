4b15614c0853385ca525c62c39f1e470d66f585a
otinu <fitshter@gmail.com>
2022-06-28 20:03:52 +0900

    [fix]newspaper3k_2.py

--- newspaper3k_2.py
+++ newspaper3k_2.py
@@ -1,6 +1,9 @@
 from newspaper import Article
 
 url = "https://diamond-rm.net/market/87934/"
+
+# 「memoize_articles = False」によって、
+#  実行時に毎回インターネット上から記事を取得する
 article = Article(url, memoize_articles = False)
 article.download()
 article.parse()
