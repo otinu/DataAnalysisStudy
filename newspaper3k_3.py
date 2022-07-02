48858c6410373a2ee323dc7df2cad42bdc319e41
otinu <fitshter@gmail.com>
2022-07-03 05:50:22 +0900

    [fix]newspaper3k_3.py

--- newspaper3k_3.py
+++ newspaper3k_3.py
@@ -5,18 +5,26 @@ website = newspaper.build(url, memoize_articles = False)
 i = 1
 for article in website.articles:
     article.download()
+
+    # 必要な情報を抽出
     article.parse()
+    # 自然言語処理の実行
     article.nlp()
+
     print("記事",str(i),":",article.title)
     print(article.url)
     print(article.summary, end="\n\n")
 
+    # 記事を10件取得
     if i > 9:
         break
     i = i + 1
 
-urls = ["https://www.itmedia.co.jp/", "https://thebridge.jp/"]
+# 複数のURLをリストに格納
+urls = ["https://www.itmedia.co.jp/",
+        "https://thebridge.jp/"]
 
+# 複数のURLをループで回す
 for url in urls:
     website = newspaper.build(url, memoize_articles = False, language='ja')
     i = 1
