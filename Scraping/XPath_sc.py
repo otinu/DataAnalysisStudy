d37c4a0906a07a989fe87cb89b10a07f52052786
otinu <fitshter@gmail.com>
2022-07-30 16:58:18 +0900

    [fix]XPath.py

--- Scraping/XPath_sc.py
+++ Scraping/XPath_sc.py
@@ -1,5 +1,6 @@
 """
-<html>
+
+```<html>
     <head>
         <title>フカセ釣り</title>
     </head>
@@ -22,36 +23,36 @@
         </p>
         <a class="lectureXXX" href="https://www.youtube.com/watch?v=Nj0Xdae98H0">ピース</a>
     </body>
-</html>
+</html>```
 
 【参考】
 https://scrapinghub.github.io/xpath-playground/
 
-/html/head/title
+``` /html/head/title ```
 ⇒<title>フカセ釣り</title>
 
-# この方法でも取得可能。ただし、そのHTML内で重複していた場合は該当部分を全て取得する。
-//title/text()
+#### この方法でも取得可能。ただし、そのHTML内で重複していた場合は該当部分を全て取得する。
+```//title/text()```
 ⇒<title>フカセ釣り</title>
 
-# text()で文章を取得できる
-/html/head/title/text()
+#### text()で文章を取得できる
+```/html/head/title/text()```
 ⇒フカセ釣り
 
-# @を使うことで、属性/属性値から探せる
-//a[@class="lecture1"]
+#### @を使うことで、属性/属性値から探せる
+```//a[@class="lecture1"]```
 ⇒<a class="lecture1" href="https://www.youtube.com/watch?v=Nj0"/>
 
-# また、ワイルドカード使用で属性値のみで探すこともできる
-//a[@*="lecture1"]
+#### また、ワイルドカード使用で属性値のみで探すこともできる
+```//a[@*="lecture1"]```
 ⇒<a class="lecture1" href="https://www.youtube.com/watch?v=Nj0"/>
 
-# contains() や not, and, or などを組み合わせて使うこともできる
-//a[not(contains(@class,"lecture1")) and contains(@class, "lectureXXX")]
+#### contains() や not, and, or などを組み合わせて使うこともできる
+```//a[not(contains(@class,"lecture1")) and contains(@class, "lectureXXX")]```
 ⇒<a class="lectureXXX" href="https://www.youtube.com/watch?v=Nj0Xdae98H0">ピース</a>
 
-# starts-with・ends-withで前方一致・後方一致で探せる
-//p[starts-with(@class,"rec")]
+#### starts-with・ends-withで前方一致・後方一致で探せる
+```//p[starts-with(@class,"rec")]```
 ⇒<p class="recent books"> <p>濁り</p> <p>集魚力</p> <p>粘り</p> </p>
 """
 
